cae0e12f6fde2ee7c6623d72b47cd169ec743e8c
TraTran23082004 <thanhtra.tran@students.mq.edu.au>
2024-09-10 16:15:13 +1000

    make the stars pop up randomly then disappear

--- dist/stars.js
+++ dist/stars.js
@@ -1,23 +1,15 @@
 export function stars() {
     let e = document.createElement("div");
-    e.setAttribute("class", "star");
+    e.classList.add("star");
     e.style.position = "absolute";
-    e.style.zIndex = "-1";
-    document.body.appendChild(e);
     e.style.left = Math.random() * +innerWidth + "px";
     e.style.top = Math.random() * innerHeight + "px";
     let size = Math.random() * 12;
-    let duration = Math.random() * 3;
     e.style.width = `${size}px`;
     e.style.height = `${size}px`;
-    e.style.borderRadius = "50%";
-    e.style.backgroundColor = "white";
-    e.style.fontSize = 12 + "px";
-    e.style.animationDuration = 2 + duration + "s";
+    document.body.appendChild(e);
     setTimeout(function () {
         document.body.removeChild(e);
     }, 5000);
 }
-setInterval(function () {
-    stars();
-}, 50);
+setInterval(stars, 50);
--- src/stars.ts
+++ src/stars.ts
@@ -1,23 +1,19 @@
 export function stars() {
     let e = document.createElement("div");
-    e.setAttribute("class", "star");
+    e.classList.add("star");
 
     e.style.position = "absolute";
-    e.style.zIndex = "-1";
 
-    document.body.appendChild(e);
     e.style.left = Math.random() * +innerWidth + "px";
     e.style.top = Math.random() * innerHeight + "px";
   
     let size = Math.random() * 12;
-    let duration = Math.random() * 3;
+  
 
     e.style.width = `${size}px`;
     e.style.height = `${size}px`;
-    e.style.borderRadius = "50%";
-    e.style.backgroundColor = "white";
-    e.style.fontSize = 12 + "px";
-    e.style.animationDuration = 2 + duration + "s";
+
+    document.body.appendChild(e);
 
     setTimeout(function () {
       document.body.removeChild(e);
@@ -26,6 +22,4 @@ export function stars() {
 
   
   
-  setInterval(function () {
-    stars();
-  }, 50);
+  setInterval(stars, 50);
