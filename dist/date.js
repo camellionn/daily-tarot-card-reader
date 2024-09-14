de8d4f3ec69668949e53665275025e8efd4e118b
TraTran23082004 <thanhtra.tran@students.mq.edu.au>
2024-09-15 01:08:25 +1000

    better date and time reading (with actual name instead of number)

--- dist/date.js
+++ dist/date.js
@@ -1,10 +1,16 @@
 export function updateDateTime() {
     const now = new Date();
-    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    const day = daysOfWeek[now.getDay()];
-    const formattedDate = now.toLocaleDateString();
-    const formattedTime = now.toLocaleTimeString();
-    const currentDateTime = `${day}, ${formattedDate}, ${formattedTime}`;
+    const options = {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit'
+    };
+    const currentDateTime = now.toLocaleDateString('en-US', options) + ' ';
     document.querySelector('#datetime').textContent = currentDateTime; //non-null assetment
 }
 setInterval(updateDateTime, 1000);
+updateDateTime();
--- src/date.ts
+++ src/date.ts
@@ -1,13 +1,18 @@
 export function updateDateTime() {
     const now = new Date();
-    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    const day = daysOfWeek[now.getDay()];
+    const options: Intl.DateTimeFormatOptions = {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit'
+    };
 
-    const formattedDate = now.toLocaleDateString();
-    const formattedTime = now.toLocaleTimeString();
-
-    const currentDateTime = `${day}, ${formattedDate}, ${formattedTime}`;
+    const currentDateTime = now.toLocaleDateString('en-US', options) + ' ';
     document.querySelector('#datetime')!.textContent = currentDateTime; //non-null assetment
 }
 
 setInterval(updateDateTime, 1000);
+updateDateTime();
